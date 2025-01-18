19f2b33149ff5408c538fe1388f9cd3e028cc263
ErenSel <erensel1303@gmail.com>
2025-01-19 02:40:44 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.BusinessLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpEgitimKampi301.BusinessLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpEgitimKampi301.BusinessLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3ba3a13-75f8-4280-a4fd-a7c0af662c56")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpEgitimKampi301.EFProject/FrmStatistics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+            
+            lblLocationCount.Text = db.Location.Count().ToString();
+            lblSumCapacity.Text = db.Location.Sum(x => x.Capacity).ToString();
+            lblGuideCount.Text = db.Guide.Count().ToString();
+            lblAverageCapacity.Text = db.Location.Average(x => (double?)x.Capacity)?.ToString("F2") ?? "0.00";
+            lblAverageLocationPrice.Text = db.Location.Average(x => (decimal?)x.Price)?.ToString("F2") + " ₺" ?? "0.00 ₺";
+            int lastCountryId = db.Location.Max(x => x.LocationId);
+            lblLastCountry.Text = db.Location.Where(x => x.LocationId == lastCountryId).Select(y => y.Country).FirstOrDefault();
+            lblCappadociaLocationCapacity.Text = db.Location.Where(x=>x.City == "Kapadokya").Select(y => y.Capacity).FirstOrDefault().ToString();
+            lblTurkiyeCapacityAverage.Text = db.Location.Where(x=>x.Country == "Türkiye").Average(y => y.Capacity).ToString();
+            var romeGuideId = db.Location.Where(x => x.City == "Roma Turistik").Select(y => y.GuideId).FirstOrDefault();
+            lblRomeGuideName.Text = db.Guide.Where(x => x.GuideId == romeGuideId).Select(y => y.GuideName + " " + y.GuideSurname).FirstOrDefault().ToString();
+
+            var maxCapacity = db.Location.Max(x => x.Capacity);
+            lblMaxCapacityLocation.Text = db.Location.Where(x => x.Capacity == maxCapacity).Select(y => y.Capacity).FirstOrDefault().ToString();
+
+            var maxPrice = db.Location.Max(x => x.Price);
+            lblMaxPriceLocation.Text = db.Location.Where(x => x.Price == maxPrice).Select(y => y.City).FirstOrDefault().ToString();
+            var guideIdByNameAysegul = db.Guide.Where(x => x.GuideName == "Ayşegül" && x.GuideSurname == "Çınar").Select(y => y.GuideId).FirstOrDefault();
+            lblAysegulCınar.Text = db.Location.Where(x => x.GuideId == guideIdByNameAysegul).Count().ToString(); 
+        }
+    }
+}
