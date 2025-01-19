8fc818b5dd1f148a4e9d4c5c823f3a0ed0b3abdf
ErenSel <erensel1303@gmail.com>
2025-01-19 17:08:32 +0300

    güncelleme

---
+++ CSharpEgitimKampi301.DataAccesLayer/EntityFramework/EfAdminDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpEgitimKampi301.DataAccesLayer.Abstract;
+using CSharpEgitimKampi301.DataAccesLayer.Repositories;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+
+namespace CSharpEgitimKampi301.DataAccesLayer.EntityFramework
+{
+    public class EfAdminDal : GenericRepository<Admin>, IAdminDal
+    {
+
+    }
+}
